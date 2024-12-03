9d411644ae7a24c5c7c183c0e970e0d85d927023
MarinEstrada <adrian.marin.estrada@gmail.com>
2024-12-03 12:50:54 -0800

    FIX: knapsack serial able to read from file

--- knapsack_serial.cpp
+++ knapsack_serial.cpp
@@ -46,53 +46,56 @@ int main(int argc, char* argv[]) {
 
     // reading stuff from a file:
 
-    // //init command line args
-    // cxxopts::Options options("Read input file",
-    //                         "Read input file for knapsack problem");
-    // options.add_options(
-    //     "custom",
-    //     {
-    //         {"fName", "File Name",
-    //         cxxopts::value<std::string>()->default_value("knapsack_input.txt")},
-    //     });
-    // auto cl_options = options.parse(argc, argv);
-    // std::string file_name = cl_options["fName"].as<std::string>();
+    //init command line args
+    cxxopts::Options options("Read input file",
+                            "Read input file for knapsack problem");
+    options.add_options(
+        "custom",
+        {
+            {"fName", "File Name",
+            cxxopts::value<std::string>()->default_value("knapsack_input.txt")},
+            {"capacity", "Capacity of the knapsack",
+            cxxopts::value<int>()->default_value("50")},
+        });
+    auto cl_options = options.parse(argc, argv);
+    std::string file_name = cl_options["fName"].as<std::string>();
+    int capacity = cl_options["capacity"].as<int>();
 
-    // // Open the file
-    // std::ifstream file(file_name); // Open the file
-    // if (!file) {
-    //     std::cerr << "Unable to open file.\n";
-    //     return 1;
-    // }
+    // Open the file
+    std::ifstream file(file_name); // Open the file
+    if (!file) {
+        std::cerr << "Unable to open file.\n";
+        return 1;
+    }
 
-    // std::string line;
-    // std::getline(file, line); // Read the first line for the number of tuples
-    // uint num_tuples = std::stoi(line);
+    std::string line;
+    std::getline(file, line); // Read the first line for the number of tuples
+    uint num_tuples = std::stoi(line);
 
-    // std::vector<int> values;
-    // std::vector<int> weights;
+    std::vector<int> values;
+    std::vector<int> weights;
 
-    // while (std::getline(file, line)) { // Read each line, one I/O per line
-    //     std::istringstream line_stream(line); // get line stream for parsing in memory
-    //     char ch;
-    //     int value, weight;
+    while (std::getline(file, line)) { // Read each line, one I/O per line
+        std::istringstream line_stream(line); // get line stream for parsing in memory
+        char ch;
+        int value, weight;
 
-    //     line_stream >> ch >> value >> ch >> weight >> ch; // Parse the tuple format (1, 2)
-    //     values.push_back(value); // Add the value to the vector
-    //     weights.push_back(weight); // add weight to the vector
-    // }
-    // file.close(); // Close the file
+        line_stream >> ch >> value >> ch >> weight >> ch; // Parse the tuple format (1, 2)
+        values.push_back(value); // Add the value to the vector
+        weights.push_back(weight); // add weight to the vector
+    }
+    file.close(); // Close the file
 
     // // output
     // for(int i = 0; i < num_tuples; ++i){
     //     std::cout << "(" << values[i] << ", " << weights[i] << ")" 
     //               << "added ints are: " << values[i] + weights[i] << std::endl;
     // }
-    std::vector<int> tmp_values = {60, 100, 120};
-    std::vector<int> tmp_weights = {10, 20, 30};
-    int capacity = 50;
-    int num_tuples = 3;
-    // cout << knapSack(capacity, weights, values, num_tuples);
-    std::cout << knapSack(capacity, tmp_weights, tmp_values, num_tuples);
+    // std::vector<int> tmp_values = {60, 100, 120};
+    // std::vector<int> tmp_weights = {10, 20, 30};
+    // int capacity = 50;
+    // int num_tuples = 3;
+    std::cout << knapSack(capacity, weights, values, num_tuples) << std::endl;
+    // std::cout << knapSack(capacity, tmp_weights, tmp_values, num_tuples);
     return 0;
 }
